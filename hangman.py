4cd616b037b9a33766eaaa9fce602257980f9842
Marcis Gailitis <marcis.gailitis@gmail.com>
2020-12-11 22:45:21 +0200

    Update code for hangman.py

--- hangman.py
+++ hangman.py
@@ -2,19 +2,26 @@ guess_word = True
 guess_letter = True
 
 while guess_word:
-    word = input('Enter the word')
-    print(word)
 
+    counter = 0
     word_out = []
     guessed_letters = set()
-
+    word = input('Enter the word for hangman game: ')
     letter_list = set(word)
-    print("".join(letter_list))
+
+    print(f'\nGuess the following word: {"* " * len(word)}')
 
     while guess_letter:
+
         print(
-            f'You have guessed following letters: {"".join(guessed_letters)}')
-        input_letter = input('enter letter: ')
+            f'You have used following letters: {", ".join(guessed_letters)}')
+        counter += 1
+        input_letter = input(
+            f'\nTry Nr {counter}. Enter a letter or quit to exit: ')
+
+        if input_letter == 'quit':
+            break
+
         if input_letter in guessed_letters:
             print('You already guessed that letter')
         else:
@@ -26,14 +33,14 @@ while guess_word:
             else:
                 word_out.append('*')
 
-        print("".join(word_out))
+        print(f'Guess the following word: {" ".join(word_out)}')
 
         if letter_list == set(word_out):
             print('Success!')
             guess_letter = False
 
-        word_out = []
+        word_out.clear()
 
-    cont = input('another?')
+    cont = input('Another (y/n)?: ')
     if cont in ('n', 'no'):
         guess_word = False
